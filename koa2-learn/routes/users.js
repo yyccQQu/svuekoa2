331667b5bfd116852f0830308dd68276199fcb13
川川 <643929860@qq.com>
2019-01-04 22:08:06 +0700

    模拟请求数据 init

--- koa2-learn/routes/users.js
+++ koa2-learn/routes/users.js
@@ -1,4 +1,5 @@
 const router = require('koa-router')()
+const Person = require('../dbs/models/person.js')
 
 router.prefix('/users')
 
@@ -9,5 +10,29 @@ router.get('/', function (ctx, next) {
 router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
+
+router.post('./addPersion', async function (ctx) {
+  const persion = new Person({
+    name: ctx.request.body.name,
+    age: ctx.request.body.age
+
+  })
+
+  let code = 0
+  try {
+    await persion.save()
+    code = 0
+  } catch (e) {
+    code = -1
+  }
+
+  await persion.save()
+  ctx.body = {
+    code: code
+  }
+
+
+})
+
 
 module.exports = router
